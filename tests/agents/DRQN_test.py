1ddc8599ed5257a677827ba85b27ef3d11862936
Piotr Januszewski <piojanuszewski@gmail.com>
2017-11-24 21:38:24 +0100

    Change EpisodicMemory to be based on Transitions

--- tests/agents/DRQN_test.py
+++ tests/agents/DRQN_test.py
@@ -2,156 +2,93 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
 import pytest
+import random
 
 from humblerl.agents import EpisodicMemory
+from humblerl.environments import Transition
 
 
-class TestEpisodicMemoryStart(object):
+class TestEpisodicMemory(object):
+
+    @staticmethod
+    def transition_fabric(
+            state=[1, 2, 3],
+            action=[7., ],
+            reward=7.,
+            next_state=[4, 5, 6],
+            is_terminal=False):
+        return Transition(state, action, reward, next_state, is_terminal)
+
+    @staticmethod
+    def transition_to_list(transition):
+        return [transition.state, transition.action,
+                transition.reward, transition.next_state]
+
+    @pytest.fixture
+    def transition(self):
+        return self.transition_fabric()
 
     @pytest.fixture
     def episodicmemory(self):
-        BUFFER_SIZE = 100
-        TRACE_LENGTH = 8
-        RANDOM_SEED = 123
-        return EpisodicMemory(
-            buffer_size=BUFFER_SIZE,
-            trace_length=TRACE_LENGTH,
-            seed=RANDOM_SEED)
-
-    def test_valid_start(self, episodicmemory):
-        INIT_STATE = [1, 2, 3]
-
-        episodicmemory.start(INIT_STATE)
-
-        assert episodicmemory._buffer_idx == 0
-        assert len(episodicmemory._episode_buffer) == 1
-        assert episodicmemory._episode_buffer[0][0] == INIT_STATE
-
-    def test_start_after_start(self, episodicmemory):
-        INIT_STATE = [1, 2, 3]
-        INIT_STATE_TWO = [7, 8, 9]
-
-        episodicmemory.start(INIT_STATE)
-        episodicmemory.start(INIT_STATE_TWO)
-
-        assert episodicmemory._buffer_idx == 1
-        assert len(episodicmemory._episode_buffer) == 2
-        assert episodicmemory._episode_buffer[0][0] == INIT_STATE
-        assert episodicmemory._episode_buffer[1][0] == INIT_STATE_TWO
-
-    def test_corrupt_start(self, episodicmemory):
-        episodicmemory._buffer_idx = 0
-
-        with pytest.raises(AssertionError):
-            episodicmemory.start([])
-
-
-class TestEpisodicMemoryStore(object):
-    INIT_STATE = [1, 2, 3]
-
-    @pytest.fixture
-    def episodicmemory(self):
-        BUFFER_SIZE = 100
-        TRACE_LENGTH = 8
-        RANDOM_SEED = 123
-        return EpisodicMemory(
-            buffer_size=BUFFER_SIZE,
-            trace_length=TRACE_LENGTH,
-            seed=RANDOM_SEED)
-
-    @pytest.fixture
-    def started_episodicmemory(self):
-        BUFFER_SIZE = 100
-        TRACE_LENGTH = 8
-        RANDOM_SEED = 123
-
         episodicmemory = EpisodicMemory(
-            buffer_size=BUFFER_SIZE,
-            trace_length=TRACE_LENGTH,
-            seed=RANDOM_SEED)
-        episodicmemory.start(self.INIT_STATE)
+            buffer_size=100,
+            trace_length=8,
+            seed=123)
 
         return episodicmemory
 
-    def test_store_before_start(self, episodicmemory):
-        with pytest.raises(ValueError):
-            episodicmemory.store([], 0., [])
+    def test_store(self, episodicmemory, transition):
+        episodicmemory.store(transition)
 
-    def test_store_after_start(self, started_episodicmemory):
-        NEXT_TRACE = [[0, 1], 7., [4, 5, 6]]
-        TARGET_TRANSITION = [self.INIT_STATE, ] + NEXT_TRACE
-
-        started_episodicmemory.store(*NEXT_TRACE)
-
-        assert started_episodicmemory._episode_buffer[0] == TARGET_TRANSITION
-
-
-class TestEpisodicMemorySample(object):
-    INIT_STATE = [1, 2, 3]
-    NEXT_TRACE = [[0, 1], 7., [4, 5, 6]]
-    TRACE_LENGTH = 2
-
-    @pytest.fixture
-    def episodicmemory(self):
-        BUFFER_SIZE = 100
-        RANDOM_SEED = 123
-        return EpisodicMemory(
-            buffer_size=BUFFER_SIZE,
-            trace_length=self.TRACE_LENGTH,
-            seed=RANDOM_SEED)
-
-    @pytest.fixture
-    def started_episodicmemory(self):
-        BUFFER_SIZE = 100
-        RANDOM_SEED = 123
-
-        episodicmemory = EpisodicMemory(
-            buffer_size=BUFFER_SIZE,
-            trace_length=self.TRACE_LENGTH,
-            seed=RANDOM_SEED)
-        episodicmemory.start(self.INIT_STATE)
-
-        return episodicmemory
+        assert episodicmemory._episode_buffer[0] == \
+            self.transition_to_list(transition)
 
     @pytest.fixture
     def filled_episodicmemory(self):
-        BUFFER_SIZE = 100
-        RANDOM_SEED = 123
+        random.seed(123)
+        TRACE_LENGTH = 7
 
         episodicmemory = EpisodicMemory(
-            buffer_size=BUFFER_SIZE,
-            trace_length=self.TRACE_LENGTH,
-            seed=RANDOM_SEED)
-        episodicmemory.start(self.INIT_STATE)
+            buffer_size=100,
+            trace_length=TRACE_LENGTH,
+            seed=123)
 
-        for _ in range(self.TRACE_LENGTH):
-            episodicmemory.store(*self.NEXT_TRACE)
+        targets = []
+        current_state = [random.randrange(100) for _ in range(5)]
+        for i in range(TRACE_LENGTH):
+            transition = \
+                self.transition_fabric(
+                    state=current_state,
+                    action=[random.random(), ],
+                    reward=float(random.randrange(-10, 10)),
+                    next_state=[random.randrange(100) for _ in range(5)],
+                    is_terminal=(i == TRACE_LENGTH - 1))
 
-        episodicmemory.start([])
+            current_state = transition.next_state
 
-        return episodicmemory
+            episodicmemory.store(transition)
+            targets.append(transition)
 
-    def test_sample_before_start(self, episodicmemory):
+        return (episodicmemory, targets)
+
+    def test_sample_before_store(self, episodicmemory):
         assert episodicmemory.sample(1) == None
 
-    def test_sample_before_store(self, started_episodicmemory):
-        assert started_episodicmemory.sample(1) == None
+    def test_sample(self, filled_episodicmemory):
+        BATCH_SIZE = 3
 
-    def test_valid_sample(self, filled_episodicmemory):
-        FIRST_TRANSITION = [self.INIT_STATE, ] + self.NEXT_TRACE
-        SECOND_TRANSITION = [self.NEXT_TRACE[2], ] + self.NEXT_TRACE
+        episodicmemory, targets = filled_episodicmemory
+        sequence = episodicmemory.sample(BATCH_SIZE)
 
-        batch = filled_episodicmemory.sample(2)
+        assert len(sequence) == len(targets)
+        for batch, target in zip(sequence, targets):
+            assert len(batch) == BATCH_SIZE
+            for transition in batch:
+                assert transition == target
 
-        assert len(batch) == 2
-        assert len(batch[0]) == 2
-        assert batch[0][0] == FIRST_TRANSITION
-        assert batch[0][1] == SECOND_TRANSITION
-        assert batch[1][0] == FIRST_TRANSITION
-        assert batch[1][1] == SECOND_TRANSITION
-
-    def test_corrupt_sample(self, filled_episodicmemory):
-        filled_episodicmemory._trace_length = self.TRACE_LENGTH + 1
+    def test_too_few_transitions(self, filled_episodicmemory):
+        episodicmemory, targets = filled_episodicmemory
+        episodicmemory._trace_length += 1
 
         with pytest.raises(ValueError):
-            filled_episodicmemory.sample(1)
+            episodicmemory.sample(1)
