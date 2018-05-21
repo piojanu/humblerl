e475dafbbcb4b9d5ad1940e5ec92f0e03ebb8727
Piotr Januszewski <piojanuszewski@gmail.com>
2018-05-21 20:04:40 +0200

    Add Planner and Policy interfaces, refactor Modeler

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from humblerl.environment import Environment, Transition
 # NOTE: It must be in this order as agent.py imports environment.py classes!
 from humblerl.agent import Agent, Vision, Policy
 from humblerl.modeler import Dynamics, PerfectDynamics, Modeler
+from humblerl.planner import Planner, Policy
