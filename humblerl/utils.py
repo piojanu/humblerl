9adb0202f35761f91bbc6d3c9ebabff39f94a1ab
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2018-11-24 21:07:26 +0100

    Add wrapper for qualitative reward environment

--- humblerl/utils.py
+++ humblerl/utils.py
@@ -1,3 +1,5 @@
+from .environments import Environment
+
 class History(object):
     """Keeps history of metrics."""
 
@@ -12,6 +14,46 @@ class History(object):
             self.history.setdefault(k, [None] * self.episode).append(metrics.get(k, None))
 
         self.episode += 1
+
+
+class QualitativeRewardEnvironment(Environment):
+    """Wrapper on Environment to achive reward only at the end of game."""
+
+    def __init__(self, env):
+        self.env = env
+
+    def render(self):
+        return self.env.render()
+
+    def reset(self, train_mode=True):
+        return self.env.reset(train_mode)
+
+    @property
+    def action_space(self):
+        return self.env.action_space
+
+    @property
+    def state_space(self):
+        return self.env.state_space
+
+    @property
+    def current_state(self):
+        return self.env.current_state
+
+    @property
+    def valid_actions(self):
+        return self.env.valid_actions()
+
+    def step(self, action):
+        state, reward, done, info = self.env.step(action)
+        if done:
+            if reward > 0:
+                reward = 1
+            else:
+                reward = -1
+        else:
+            reward = 0
+        return state, reward, done, info
 
 
 def unpack(value, number=2, default=None):
