ad2e15147b7b6ad40e584f6b94b61a34cb1e7adf
Piotr Januszewski <piojanuszewski@gmail.com>
2018-12-29 10:43:32 +0100

    Refactor Q-Learning sample, tune hyper-params

--- samples/q_learning.py
+++ samples/q_learning.py
@@ -1,19 +1,18 @@
 import argparse
 import humblerl as hrl
-import numpy as np
-
 from humblerl import Callback, Mind
+import numpy as np
 
 
 class TabularQLearning(Mind, Callback):
-    def __init__(self, nstates, nactions, learning_rate=0.9, decay_steps=300, discount_factor=0.95):
+    def __init__(self, nstates, nactions, learning_rate, decay_steps, discount_factor):
         # Store training parameters
         self._lr = learning_rate
         self._decay = decay_steps
         self._gamma = discount_factor
         self._episode_count = 1
         self._return = 0
-        self._running_avg = 0
+        self.running_avg = 0
 
         # Initialize Q-table
         self.Q = np.zeros((nstates, nactions), dtype=np.float)
@@ -25,9 +24,6 @@ class TabularQLearning(Mind, Callback):
 
     def on_episode_start(self, episode, train_mode):
         self._return = 0
-
-    def on_episode_end(self, episode, train_mode):
-        self._running_avg = 0.01 * self._return + 0.99 * self._running_avg
 
     def on_step_taken(self, step, transition, info):
         # Add reward to return
@@ -49,19 +45,21 @@ class TabularQLearning(Mind, Callback):
         if transition.is_terminal:
             self._episode_count += 1
 
+    def on_episode_end(self, episode, train_mode):
+        self.running_avg = 0.01 * self._return + 0.99 * self.running_avg
+
     @property
     def metrics(self):
-        return {"avg. return": self._running_avg}
-
+        return {"avg. return": self.running_avg}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='HumbleRL tabular Q-Learning sample')
-    parser.add_argument('--episodes', type=int, default=500, metavar='N',
-                        help='number of episodes to train (default: 500)')
-    parser.add_argument('--lr', type=float, default=0.9, metavar='LR',
-                        help='learning rate (default: 0.9)')
-    parser.add_argument('--decay', type=int, default=300, metavar='N',
-                        help='exploration decay steps (default: 300)')
+    parser.add_argument('--episodes', type=int, default=865, metavar='N',
+                        help='number of episodes to train (default: 865)')
+    parser.add_argument('--lr', type=float, default=0.75, metavar='LR',
+                        help='learning rate (default: 0.75)')
+    parser.add_argument('--decay', type=int, default=400, metavar='N',
+                        help='exploration decay steps (default: 400)')
     parser.add_argument('--gamma', type=float, default=0.95, metavar='G',
                         help='discount factor (default: 0.95)')
     args = parser.parse_args()
@@ -74,8 +72,8 @@ if __name__ == "__main__":
                             discount_factor=args.gamma)
 
     # Seed env and numpy
-    np.random.seed(77)
-    env.env.seed(77)
+    np.random.seed(7)
+    env.env.seed(7)
 
     # Run training
     hrl.loop(env, mind, n_episodes=args.episodes, callbacks=[mind])
